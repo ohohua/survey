91d120ccb877269e2139f84919e751b227a24b15
ohohua <ohohua@163.com>
2025-02-25 21:22:33 +0800

    feat(page): Login page

--- apps/admin/src/pages/Login.tsx
+++ apps/admin/src/pages/Login.tsx
@@ -1,5 +1,61 @@
-function Login() {
-  return <>login</>
+import type { FormProps } from 'antd'
+import { REGISTER_PATHNAME } from '@/router'
+import { Button, Flex, Form, Input } from 'antd'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import s from './Register.module.scss'
+
+interface FieldType {
+  username?: string
+  password?: string
+}
+
+const onFinish: FormProps<FieldType>['onFinish'] = (_values) => {
+  // console.log('Success:', values)
+}
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (_errorInfo) => {
+  // console.log('Failed:', errorInfo)
+}
+
+const Login: React.FC = () => {
+  return (
+    <div className={s.container}>
+      <Form
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          label="账号"
+          name="username"
+          rules={[{ required: true, message: '请输入账号' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="密码"
+          name="password"
+          rules={[{ required: true, message: '请输入密码' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Flex justify="space-between">
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+            <Link to={`/${REGISTER_PATHNAME}`}>没有账号，去注册</Link>
+          </Flex>
+        </Form.Item>
+      </Form>
+    </div>
+
+  )
 }
 
 export default Login
