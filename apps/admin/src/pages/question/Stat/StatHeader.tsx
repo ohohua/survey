99f7542bac55755f8914c37f958b19defd1bef1d
ohohua <ohohua@163.com>
2025-03-29 21:05:59 +0800

    feat: statistics page header

--- apps/admin/src/pages/question/Stat/StatHeader.tsx
+++ apps/admin/src/pages/question/Stat/StatHeader.tsx
@@ -1,21 +1,37 @@
-import { LeftOutlined } from '@ant-design/icons'
-import { Button, Flex } from 'antd'
-import { useNavigate } from 'react-router-dom'
+import { CopyOutlined, LeftOutlined, QrcodeOutlined } from '@ant-design/icons'
+import { Button, Flex, Input, message, Popover } from 'antd'
+import { useNavigate, useParams } from 'react-router-dom'
 import s from './StatHeader.module.scss'
 
 function StatHeader() {
   const nav = useNavigate()
+  const { id = '' } = useParams()
+
   function handleBack() {
     nav({ pathname: '/manage/list' })
   }
+
+  function handleCopy() {
+    message.success('复制成功')
+  }
+
+  function handleEdit() {
+    nav({ pathname: `/question/edit/${id}` })
+  }
+
+  function renderQrcode() {
+    return <div className={s.qrcode}></div>
+  }
   return (
     <Flex className={s.header} justify="space-between" align="center">
-      <div>
-        <Button type="link" icon={<LeftOutlined />} onClick={handleBack}> 返回 </Button>
-      </div>
+      <Button type="link" icon={<LeftOutlined />} onClick={handleBack}> 返回 </Button>
       <Flex gap="small">
-        <Button type="primary">编辑</Button>
+        <Input addonAfter={<CopyOutlined onClick={handleCopy} />} placeholder="问卷链接" />
+        <Popover content={renderQrcode()} trigger="click">
+          <Button style={{ flexShrink: 0 }} icon={<QrcodeOutlined />} />
+        </Popover>
       </Flex>
+      <Button type="primary" onClick={handleEdit}>编辑</Button>
     </Flex>
   )
 }
