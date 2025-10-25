207b64b7092ba53f4ec9786124b0cf8802d03071
ohohua <ohohua@163.com>
2025-10-25 15:42:32 +0800

    feat: logout

--- apps/admin/src/components/UserInfo.tsx
+++ apps/admin/src/components/UserInfo.tsx
@@ -1,16 +1,24 @@
 import { LOGIN_PATHNAME } from '@/router'
+import { useAuthStore } from '@/store/useAuthStore'
 import { Button } from 'antd'
 import { useNavigate } from 'react-router-dom'
 
 function UserInfo() {
+  const { token } = useAuthStore()
   const nav = useNavigate()
   const handleLogin = () => {
+    nav({ pathname: LOGIN_PATHNAME })
+  }
+  const handleLogout = () => {
+    useAuthStore.getState().setToken('')
     nav({ pathname: LOGIN_PATHNAME })
   }
 
   return (
     <>
-      <Button type="link" onClick={handleLogin}> 登录 </Button>
+      {token
+        ? <Button type="link" onClick={handleLogout}> 退出登录 </Button>
+        : <Button type="link" onClick={handleLogin}> 登录 </Button>}
     </>
   )
 }
